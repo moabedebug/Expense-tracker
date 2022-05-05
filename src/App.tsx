84c5c275a3febf430dfe4a92c47cc1ccf75d122c
Moabe <moabeluis2018@gmail.com>
2022-05-05 20:27:40 -0300

    Fazendo Estados

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useState } from 'react'
 import * as C from './App.styles'
+import { Item } from './types/Item'
+import { Category } from './types/Category'
+import { categories } from './data/categories' 
+import { items } from './data/items'
+import { getCurrentMonth } from './helpers/dateFilter'
 
 const App = () => {
+  const [list, setList] = useState(items)
+  const [filteredList, setFilteredList] = useState<Item[]>([])
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth())
+
   return(
     <C.Container>
       <C.Header>
